0c8a59567db68484c8fc310521d4fd59f9510a6e
Edward Zaplotynskyi <edward.downtown@gmail.com>
2023-12-29 13:39:30 -0500

    setup testNG and fixed chromedriver issue

---
+++ src/TestPackage/testng_prjoect.java
@@ -0,0 +1,27 @@
+package TestPackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.testng.annotations.Test;
+
+public class testng_prjoect {
+	
+	@Test
+	public void TestNG() {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "/Users/mgid/Downloads/chromedriver-mac-x64/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://booking.com");
+		
+		
+		System.out.println("Executing some code");
+		
+		System.out.println(driver.getTitle());
+		
+//		driver.quit();
+	}
+
+}
+
+///mgid/Documents/chromedriver_mac64/chromedriver
